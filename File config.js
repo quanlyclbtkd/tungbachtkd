373a0edf6bdff79a621dcbffacfd84d44dadde9c
quanlyclbtkd <truongtinhtkd92@gmail.com>
2026-05-01 22:25:58 +0700

    Add files via upload

---
+++ File config.js
@@ -0,0 +1,20 @@
+// File: config.js (Của CLB Tùng Bách)
+window.CLUB_CONFIG = {
+    firebase: {
+        apiKey: "AIzaSyAwSmopeOLLuN6yQChHCGvpWSEeKBf6ci8",
+        authDomain: "tungbach-a3a5b.firebaseapp.com",
+        projectId: "tungbach-a3a5b",
+        storageBucket: "tungbach-a3a5b.firebasestorage.app",
+        messagingSenderId: "1097901460689",
+        appId: "1:1097901460689:web:2e25eaf7d377be191349f0"
+    },
+    bank: {
+        id: "VIETINBANK",
+        accountNo: "108880450814",
+        accountName: "PHAM QUANG TUNG - CLB TAEKWONDO TÙNG BÁCH"
+    },
+    passwords: {
+        admin: "tkdtungbach2112024",
+        viewer: "Tuyet1992"
+    }
+};
